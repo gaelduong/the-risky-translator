0fd491958c49b80490bdfc6107b551aa5aa0691b
Gael Duong <mr_jupipro@yahoo.com>
2023-02-20 01:33:28 +0700

    Display Header under SafeArea (PWA)

--- src/ui/components/Header/index.jsx
+++ src/ui/components/Header/index.jsx
@@ -4,9 +4,14 @@ import { AppContext } from '../../../context'
 const Header = () => {
   const { state } = useContext(AppContext)
 
-  if (state.money < 0) return <div> Money: -${state.money * -1}</div>
+  const displayMoney =
+    state.money < 0 ? `-$${state.money * -1}` : `$${state.money}`
 
-  return <div> Money: ${state.money}</div>
+  return (
+    <div style={{ marginTop: 'env(safe-area-inset-top)' }}>
+      Money: {displayMoney}
+    </div>
+  )
 }
 
 export default Header
