f1b6e4941e2d6d050083eaab835ae1a505821b09
Gael Duong <mr_jupipro@yahoo.com>
2023-02-19 23:32:39 +0700

    Update App and add some css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import CityMap1 from './ui/screens/CityMap1'
-import CityMap2 from './ui/screens/CityMap2'
-import Work from './ui/screens/Work'
 import './algorithm'
+import Map from './ui/screens/Map'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/city1" element={<CityMap1 />} />
-      <Route path="/city2" element={<CityMap2 />} />
-      <Route path="/work" element={<Work />} />
-      {/* <div className="App">
-      <h1>Test</h1>
-    </div> */}
-    </Routes>
+    <div className="App">
+      <Map />
+    </div>
   )
 }
 
