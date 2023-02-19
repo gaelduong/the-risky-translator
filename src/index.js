5dec661e37aa49b3a772a66ff1a01e17d29b3e25
Gael Duong <mr_jupipro@yahoo.com>
2023-02-20 00:27:55 +0700

    Set up PWA

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import { AppProvider } from './context'
 import App from './App'
 
@@ -22,3 +22,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+
+serviceWorkerRegistration.register()
