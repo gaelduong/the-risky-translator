d25cdabbb8dfdddbade2e7fd945b58a9a4c2e55c
Gael Duong <mr_jupipro@yahoo.com>
2023-03-01 17:46:27 +0700

    Test sizing elements

--- src/ui/screens/Town/index.tsx
+++ src/ui/screens/Town/index.tsx
@@ -1,16 +1,24 @@
 import { Link } from 'react-router-dom'
-
+import { creatureImage } from '@Assets/images'
 const Town = () => {
   return (
     <div style={{ touchAction: 'none' }}>
       <Link to="/profile">Profile</Link>
       <Link to="/monster-map">MonsterMap</Link>
       <Link to="/training">Training</Link>
-      <div className="test-container">
-        <div className="test">Test</div>
-        <div className="test">Test</div>
-        <div className="test">Test</div>
+      <h2> </h2>
+      <div className="container">
+        <button className="leave">Leave</button>
+        <img className="creature" src={creatureImage} alt="s" />
+        <h2 className="header">Time</h2>
+        <button className="yes-button">Yes</button>
+        <button className="no-button">No</button>
       </div>
+      {/* <div className="test-container">
+        <div className="test">Test</div>
+        <div className="test">Test</div>
+        <div className="test">Test</div>
+      </div> */}
     </div>
   )
 }
